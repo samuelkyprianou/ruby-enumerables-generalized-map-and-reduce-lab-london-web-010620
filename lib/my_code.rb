443f55e9ff2b52db36e0c066fde5b3dac69614fc
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 17:47:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map(array)
   end
   new
 end
+
+def map(array, starting_point=0)
+  value = tota;
+    counter = 0
+    while counter < array.size 
+    new << yield(array[counter])
+    counter += 1
+  end
+  new
+end
