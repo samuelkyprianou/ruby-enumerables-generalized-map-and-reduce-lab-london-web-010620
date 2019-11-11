8ea00901bf5a3e34d31122a0287be9f3d95f123f
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 17:59:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(array, starting_point=0)
     value = starting_point
     counter = 0
     while counter < array.size 
-    yield(value
+    yield(value, 
     counter += 1
   end
   value
