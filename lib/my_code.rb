5be0911f0b4e8fdf0b248f07358146126110b37d
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 17:41:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@ def map(array)
   new = []
     counter = 0
     while counter < array.size 
-  new << yield(array[counter])
-  counter += 1
-end
+    new << yield(array[counter])
+    counter += 1
+  end
 end
 
